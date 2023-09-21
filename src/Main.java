c5090e79becc227ed1b1250fb51fe6344cce3f5c
Audyari <audy123ari@gmail.com>
2023-09-21 15:59:27 +0700

    Input TextJam 15.59

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(args[0]);
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(input.nextLine());
 
         }
 }
