9013e7d8b5984cc0ccc1a90ac57ff4fd847aed37
Audyari <audy123ari@gmail.com>
2023-09-22 10:19:16 +0700

    Imput Output  jam 10.19

---
+++ src/Input_Output.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Input_Output {
+    public static void main(String[] args) {
+       /* Scanner angka = new Scanner(System.in);
+        int data1 = angka.nextInt();
+        int data2 = angka.nextInt();
+
+        System.out.println(data1+data2);
+*/
+        int a = 257;
+        byte b = (byte)a;
+
+        System.out.println(b);
+    }
+}
